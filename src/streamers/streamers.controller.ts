5fb250d92f4b1157afd3591d93cd9bf12eec0c78
Dawid Marek <dawid.marek95x@gmail.com>
2023-06-30 01:50:05 +0200

    Added POST endpoint support for streamers

--- src/streamers/streamers.controller.ts
+++ src/streamers/streamers.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+  getSchemaPath,
+} from '@nestjs/swagger';
+import { Controller, Post, Body } from '@nestjs/common';
+import { StreamersService } from './streamers.service';
+import { Streamer } from 'src/entities/streamer.entity';
+import { ResponseMetadataModel } from 'src/swagger/ResponseMetadata.model';
+import { CreateStreamerDto } from './dto/create-streamer.dto';
 
+@ApiTags('streamers')
 @Controller('streamers')
-export class StreamersController {}
+export class StreamersController {
+  constructor(private readonly streamersService: StreamersService) {}
+
+  @Post()
+  @ApiOperation({
+    summary: 'Creates a new streamer',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The streamer has been successfully created.',
+    schema: {
+      type: 'object',
+      properties: {
+        data: {
+          $ref: getSchemaPath(Streamer),
+        },
+        meta: {
+          $ref: getSchemaPath(ResponseMetadataModel),
+        },
+      },
+    },
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Incorrect data or data type provided.',
+  })
+  @ApiResponse({
+    status: 409,
+    description: 'The streamer with the given details already exists.',
+  })
+  @ApiResponse({
+    status: 500,
+    description: 'Internal server error.',
+  })
+  async createStreamer(@Body() createStreamerDto: CreateStreamerDto) {
+    const createdStreamer = await this.streamersService.createOne(
+      createStreamerDto,
+    );
+
+    return {
+      data: createdStreamer,
+      meta: {
+        status: 'success',
+      },
+    };
+  }
+}
--- src/streamers/streamers.service.ts
+++ src/streamers/streamers.service.ts
@@ -1,3 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import { StreamersRepository } from 'src/repositories/streamers.repository';
+import { CreateStreamerDto } from './dto/create-streamer.dto';
+import { Streamer } from 'src/entities/streamer.entity';
+import { CheckStreamerExistenceDto } from './dto/check-streamer-existence.dto';
+
 @Injectable()
-export class StreamersService {}
+export class StreamersService {
+  constructor(private readonly streamersRepository: StreamersRepository) {}
+
+  async createOne(streamerDto: CreateStreamerDto) {
+    await this.checkStreamerExistence(streamerDto);
+
+    const streamer = new Streamer();
+    const INITIAL_VOTES = 0;
+
+    streamer.name = streamerDto.name;
+    streamer.pseudonym = streamerDto.pseudonym ?? null;
+    streamer.description = streamerDto.description;
+    streamer.streamingPlatform = streamerDto.streamingPlatform;
+    streamer.avatarUrl = streamerDto.avatarUrl;
+    streamer.positiveVotes = INITIAL_VOTES;
+    streamer.negativeVotes = INITIAL_VOTES;
+
+    return await this.streamersRepository.save(streamer);
+  }
+
+  private async checkStreamerExistence(streamerDto: CheckStreamerExistenceDto) {
+    const checkedStreamer = await this.streamersRepository.findOne({
+      where: {
+        name: streamerDto.name,
+        streamingPlatform: streamerDto.streamingPlatform,
+      },
+    });
+
+    if (checkedStreamer) {
+      throw new ConflictException(
+        'The streamer with the given details already exists.',
+      );
+    }
+  }
+}
